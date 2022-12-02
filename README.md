a5dfc8c515e511de2f5c1a14c17fb74fcd6d72ac
anattolia <bustamanter.antonia@gmail.com>
2022-12-02 15:37:12 -0500

    ✨ bot poeta

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# Plantilla Vite
+# Licuadoras luminosas
 
 ![Estilo Código](https://github.com/enflujo/enflujo-plantilla-vite/actions/workflows/estilo-codigo.yml/badge.svg)
 ![Tamaño](https://img.shields.io/github/repo-size/enflujo/enflujo-plantilla-vite?color=%235757f7&label=Tama%C3%B1o%20repo&logo=open-access&logoColor=white)
 ![Licencia](https://img.shields.io/github/license/enflujo/enflujo-plantilla-vite?label=Licencia&logo=open-source-initiative&logoColor=white)
+
+Un bot poeta basado en el libro _Licuadoras luminosas_ de La Jaula Publicasiones, recopilación de los poemas espontáneos de Enrique Peñalosa.
+
+Bogotá, peor para todxs.
--- src/programa.js
+++ src/programa.js
@@ -1,3 +1,45 @@
 import './scss/estilos.scss';
 
-console.log('..:: EnFlujo ::..');
+import { titulos, frases } from './textos';
+
+let indicesElegidos = [];
+
+const poema1 = document.getElementById('poema1');
+const titulo1 = document.getElementById('titulo1');
+const boton = document.getElementById('boton');
+
+function numeroRandom(min, max) {
+  return Math.ceil(Math.random() * (max - min) + min);
+}
+
+function generarVerso() {
+  const random = numeroRandom(1, frases.length - 1);
+
+  if (!indicesElegidos.includes(random)) {
+    indicesElegidos.push(random);
+    if (frases[random]) {
+      return frases[random];
+    }
+  } else {
+    return frases[0];
+  }
+}
+
+function generarPoema() {
+  indicesElegidos = [];
+  const indiceTitulo = numeroRandom(0, titulos.length);
+
+  const verso1 = generarVerso();
+  const verso2 = generarVerso();
+  const verso3 = generarVerso();
+  const verso4 = generarVerso();
+
+  poema1.innerText = verso1 + '\n' + verso2 + '\n' + verso3 + '\n' + verso4 + '.';
+  titulo1.innerText = titulos[indiceTitulo] ? titulos[indiceTitulo] : 'Sin título';
+}
+
+boton.addEventListener('click', function () {
+  generarPoema();
+});
+
+generarPoema();
